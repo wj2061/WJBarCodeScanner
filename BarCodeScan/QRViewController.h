24d189643d0e4344d0d0884aa51da201428d1528
wj2061 <wj2061@163.com>
2015-12-04 18:11:53 +0800

    add reference

---
+++ BarCodeScan/QRViewController.h
@@ -0,0 +1,15 @@
+//
+//  QRViewController.h
+//  QRWeiXinDemo
+//
+//  Created by lovelydd on 15/4/25.
+//  Copyright (c) 2015年 lovelydd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRViewController : UIViewController
+
+
+
+@end
